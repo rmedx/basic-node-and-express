29087b45426b488be8b9875e08275a41ff0f5141
rmedx <65210483+rmedx@users.noreply.github.com>
2021-08-13 14:54:47 -0400

    added .env file with variable for style

--- myApp.js
+++ myApp.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var app = express();
+require('dotenv').config();
 console.log("Hello World");
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
 app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/json", (req, res) => res.json({"message": "Hello json"}));
+app.get("/json", function(req, res){
+    let message;
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        message = "HELLO JSON";
+    } else {
+        message = "Hello json";
+    }
+    res.json({"message": message});
+});
 
 
 
