cba9c64c9f406fef05102ec7e300086b202f4751
rmedx <65210483+rmedx@users.noreply.github.com>
2021-08-13 12:29:10 -0400

    myApp added sendFile

--- myApp.js
+++ myApp.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
 console.log("Hello World");
-app.get("/", (req, res) => res.send("Hello Express"));
+app.get("/", function(req, res) {
+
+    res.sendFile(__dirname + "/views/index.html");
+});
 
 
 
