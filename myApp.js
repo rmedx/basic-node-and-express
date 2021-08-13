c6f10f98d746900b82f00552c3d92a63564324a6
rmedx <65210483+rmedx@users.noreply.github.com>
2021-08-13 14:09:06 -0400

    changed to arrow functions

--- myApp.js
+++ myApp.js
@@ -1,18 +1,12 @@
 var express = require('express');
 var app = express();
 console.log("Hello World");
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/views/index.html");
-});
+app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
 app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/json", function(req, res) {
-    res.json({"message": "Hello json"});
-});
-
-
+app.get("/json", (req, res) => res.json({"message": "Hello json"}));
 
 
 
